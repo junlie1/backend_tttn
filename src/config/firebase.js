568d8514673f9eaaff5ad732328e50699bd6746e
junlie1 <nlt24304@gmail.com>
2025-04-07 18:34:22 +0700

    update .env

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,14 @@
-
 const admin = require('firebase-admin');
-const serviceAccount = require('../../nhom18-tttn-firebase-adminsdk-5bys1-d63e10180a.json');
 
+require('dotenv').config();
+
+// Parse nội dung từ biến môi trường FIREBASE_CONFIG (JSON dạng chuỗi 1 dòng)
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+
+// Chuyển lại private_key thành dạng đúng (xuống dòng)
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
+// Khởi tạo Firebase Admin
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
@@ -9,4 +16,4 @@ admin.initializeApp({
 const db = admin.firestore();
 const auth = admin.auth();
 
-module.exports = { admin, db,auth };
+module.exports = { admin, db, auth };
